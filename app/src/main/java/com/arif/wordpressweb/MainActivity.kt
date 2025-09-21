9a99df04d5c733755063d4c096be8c51f59d15a5
ARIF BIN HAMID <hamid22205101138@diu.edu.bd>
2025-09-21 21:51:41 +0600

    INITIAL COMMIT

---
+++ app/src/main/java/com/arif/wordpressweb/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.arif.wordpressweb
+
+import android.net.http.SslError
+import android.os.Bundle
+import android.webkit.SslErrorHandler
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.arif.wordpressweb.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.weBview.settings.javaScriptEnabled = true
+
+        binding.weBview.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler?,
+                error: SslError?
+            ) {
+                // Ignore SSL certificate errors
+                handler?.proceed() // Ignore SSL certificate errors
+                // handler?.cancel() // If you want to cancel loading the page
+            }
+
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        binding.weBview.loadUrl("https://studentportal.diu.edu.bd/")
+    }
+
+    }
